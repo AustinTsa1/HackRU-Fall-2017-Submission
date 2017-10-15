f2704fdf26e35744742407a90f3815db362e72b7
davidt314159 <davidt1618@gmail.com>
2017-10-15 01:42:47 -0400

    Fix CSS selectors

--- public/signup.js
+++ public/signup.js
@@ -1,23 +1,23 @@
 var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function validate(){
-	if (document.querySelector('input [name=email]').value == null){
-		document.querySelector('div #form-alert').innerHTML = "Please enter a valid email.";
-		document.querySelector('div #form-alert').style.display = "inline";
+	if (document.querySelector('input[name=email]').value == null){
+		document.querySelector('div#form-alert').innerHTML = "Please enter a valid email.";
+		document.querySelector('div#form-alert').style.display = "inline";
 		return false;
-	} else if (document.querySelector('input [name=password]').value == null){
-		document.querySelector('div #form-alert').innerHTML = "Please enter a password.";
-		document.querySelector('div #form-alert').style.display = "inline";
+	} else if (document.querySelector('input[name=password]').value == null){
+		document.querySelector('div#form-alert').innerHTML = "Please enter a password.";
+		document.querySelector('div#form-alert').style.display = "inline";
 		return false;
 	}
 
-	var email = document.querySelector('input [name=email]').value;
+	var email = document.querySelector('input[name=email]').value;
 
 	if (re.test(email)){
-		document.querySelector('form #signup-form').submit();
+		document.querySelector('form#signup-form').submit();
 	} else {
-		document.querySelector('div #form-alert').innerHTML = "Please enter a valid email.";
-		document.querySelector('div #form-alert').style.display = "inline";
+		document.querySelector('div#form-alert').innerHTML = "Please enter a valid email.";
+		document.querySelector('div#form-alert').style.display = "inline";
 	}
 }
 
