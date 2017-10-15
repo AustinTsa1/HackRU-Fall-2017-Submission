4111433a60c6c3f1d5c0dafe3e7171e2a2c75834
Aaron Kau <aaron.kau@rutgers.edu>
2017-10-14 22:20:24 -0400

    Add EJS and Passport.

--- app.js
+++ app.js
@@ -1,8 +1,108 @@
-const express = require('express');
-const app = express();
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./localStrategy')(passport); // pass passport for configuration
+
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'callrecorder' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// =====================================
+// HOME PAGE (with login links) ========
+// =====================================
+app.get('/', function(req, res) {
+    res.render('index.ejs'); // load the index.ejs file
 });
+
+// =====================================
+// LOGIN ===============================
+// =====================================
+// show the login form
+app.get('/login', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs', { message: req.flash('loginMessage') }); 
+});
+
+// process the login form
+// app.post('/login', do all our passport stuff here);
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+app.get('/signup', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+// process the signup form
+// app.post('/signup', do all our passport stuff here);
+
+// =====================================
+// PROFILE SECTION =====================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+    });
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+// launch ======================================================================
+app.listen(port);
+console.log('Listening on port: ' + port);
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url: "mongodb://callrecorder:password@ds037215.mlab.com:37215/callrecorder"
+};
