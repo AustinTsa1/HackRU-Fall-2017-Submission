09e11f98bc854545052ef17e57ea76d4c69385d1
davidt314159 <davidt1618@gmail.com>
2017-10-15 01:55:57 -0400

    Testing stuff

--- public/signup.js
+++ public/signup.js
@@ -1,14 +1,15 @@
-var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 function validate(){
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	console.log("button clicked");
+
 	if (document.querySelector('input[name=email]').value == null){
-		document.querySelector('div#form-alert').innerHTML = "Please enter a valid email.";
+		document.querySelector('div#form-alert').innerHTML = "Please enter an email.";
 		document.querySelector('div#form-alert').style.display = "inline";
-		return false;
+		return;
 	} else if (document.querySelector('input[name=password]').value == null){
 		document.querySelector('div#form-alert').innerHTML = "Please enter a password.";
 		document.querySelector('div#form-alert').style.display = "inline";
-		return false;
+		return;
 	}
 
 	var email = document.querySelector('input[name=email]').value;
@@ -19,6 +20,7 @@ function validate(){
 		document.querySelector('div#form-alert').innerHTML = "Please enter a valid email.";
 		document.querySelector('div#form-alert').style.display = "inline";
 	}
+	return;
 }
 
 document.querySelector('button#submit-button').onclick = validate();
